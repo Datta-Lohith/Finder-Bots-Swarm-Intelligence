125b221640b07182657c6a21073738e4aa562eaf
Datta Lohith <gdattalohith@gmail.com>
2024-12-10 22:14:18 -0500

    Task 2.20.0	Added swarm spawning

--- launch/warehouse.launch.py
+++ launch/warehouse.launch.py
@@ -1,6 +1,8 @@
 from launch import LaunchDescription
+from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -20,6 +22,20 @@ def generate_launch_description():
     gazebo_models_path = os.path.join(pkg_share, 'models')
     os.environ['GAZEBO_MODEL_PATH'] = gazebo_models_path
 
+    # Robot description
+    urdf_path = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'models',
+        'turtlebot3_' + TURTLEBOT3_MODEL,
+        'model.sdf'
+    )
+    robot_desc = os.path.join( get_package_share_directory('turtlebot3_gazebo'), 'urdf', 'turtlebot3_waffle_pi.urdf')
+    with open(robot_desc, 'r') as urdf_file:
+        robot_desc_content = urdf_file.read()
+
+    # Use simulation time
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
     # Gazebo launch
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -33,8 +49,42 @@ def generate_launch_description():
         )
     )
     
+    # Robot State Publisher Node
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc_content, 'use_sim_time': use_sim_time}]
+    )
+
+    # Spawning multiple robots
+    spawn_robot_nodes = []
+    robot_pose = [(9.0, 28.0), (0.0, -1.0), (1.0, 20.0), (7.0, 2.0), (20.0, 3.0),
+                  (23.0, -3.0), (3.0, 30.0), (20.0, 18.0), (22.0, 30.0), (10.0, -6.0),
+                  (11.0, 12.0), (0.0, 12.0)
+                  ]
+
+    for i in range(len(robot_pose)):
+        robot_name = f"robot_{i}"
+        spawn_robot_nodes.append(Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', robot_name,
+                '-file', urdf_path,
+                '-x', str(robot_pose[i][0]),
+                '-y', str(robot_pose[i][1]),
+                '-z', '0.01',
+                '-robot_namespace', robot_name
+            ],
+            output='screen'
+        ))
+
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='false'),
         gzserver_cmd,
         gzclient_cmd,
+        robot_state_publisher_node,
+        *spawn_robot_nodes  # Add all robot spawn nodes to the LaunchDescription
     ])
